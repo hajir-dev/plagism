2175832c8ff9ec9525fcd1f08e78b773410daff7
Muhajir <69819957+hajirr@users.noreply.github.com>
2024-02-11 16:50:30 +0700

    feat: save submitted data to json file

--- app/api/convert/route.ts
+++ app/api/convert/route.ts
@@ -1,25 +1,64 @@
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
+
+function getCurrentTime(): string {
+  return new Date().toISOString();
+}
+
+function convertText(text: string): string {
+  return text.replace(/i/g, "і").replace(/o/g, "о");
+}
+
+function saveDataToFile(text: string): void {
+  const currentTime = getCurrentTime();
+
+  const filePath = "data/posted_data.json";
+  let existingData = [];
+  if (existsSync(filePath)) {
+    const jsonData = readFileSync(filePath, "utf8");
+    existingData = JSON.parse(jsonData);
+  }
+
+  const newData = { originalText: text, submittedAt: currentTime };
+  existingData.push(newData);
+
+  // Pastikan direktori 'data' ada
+  const dataDir = "data";
+  if (!existsSync(dataDir)) {
+    mkdirSync(dataDir);
+  }
+
+  // Simpan seluruh data ke dalam file .json
+  const jsonData = JSON.stringify(existingData);
+  writeFileSync(filePath, jsonData);
+}
 
 export async function POST(request: Request) {
-    try {
-        let body = await request.json();
-        let text: string = body.text;
+  try {
+    let body = await request.json();
+    let text: string = body.text;
 
-        // Lakukan penggantian huruf
-        const convertedText = text.replace(/i/g, 'і').replace(/o/g, 'о')
+    // Lakukan penggantian huruf
+    const convertedText = convertText(text);
 
-        return new Response(JSON.stringify({ result: convertedText }), {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            status: 201
-        });
-    } catch (error) {
-        console.error(error);
-        return new Response(JSON.stringify({ message: 'Terjadi kesalahan server' }), {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            status: 500
-        });
-    }
+    // Simpan data ke dalam file .json
+    saveDataToFile(text);
+
+    return new Response(JSON.stringify({ result: convertedText }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 201,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ message: "Terjadi kesalahan server" }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        status: 500,
+      }
+    );
+  }
 }
