321e337fbe4972b7bfc1df328b11e9dd54983cc6
Muhajir <69819957+hajirr@users.noreply.github.com>
2024-02-12 22:48:26 +0700

    feat: loading state

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -4,11 +4,14 @@ import { useRecoilValue, useSetRecoilState } from "recoil";
 import { acceptTOSState, convertedTextState, textState } from "../_store/store";
 import { convertText } from "../_services/convert";
 import { toast, ToastOptions } from "react-toastify";
+import { useState } from "react";
+import { PiSpinnerGap } from "react-icons/pi";
 
 const Button = () => {
   const setConvertedText = useSetRecoilState(convertedTextState);
   const text = useRecoilValue(textState);
   const isAcceptTOS = useRecoilValue(acceptTOSState);
+  const [isLoading, setIsLoading] = useState(false);
 
   const toastOptions: ToastOptions = {
     position: "bottom-center",
@@ -21,6 +24,7 @@ const Button = () => {
   };
 
   const handleSubmit = () => {
+    setIsLoading(true);
     convertText(text)
       .then((response: { result: string | ((currVal: string) => string) }) => {
         setConvertedText(response.result);
@@ -28,16 +32,23 @@ const Button = () => {
       })
       .catch((e: any) => {
         toast.error("Conversion Error", toastOptions);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
   };
 
   return (
     <button
-      disabled={!isAcceptTOS}
+      disabled={!isAcceptTOS || isLoading}
       onClick={handleSubmit}
       className="border w-full py-4 rounded-lg  duration-300 border-gray-300 hover:shadow disabled:opacity-10 dark:hover:shadow-white dark:focus:shadow-white"
     >
-      Convert
+      {isLoading ? (
+        <PiSpinnerGap className="mx-auto animate-spin" size={20} />
+      ) : (
+        "Convert"
+      )}
     </button>
   );
 };
